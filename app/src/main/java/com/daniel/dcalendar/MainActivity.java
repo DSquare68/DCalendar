a82f5046e379eaf40e1e0d8637981cb9a025b93c
DanielDob <35843305+DanielDob@users.noreply.github.com>
2018-09-16 17:58:39 +0200

    Added day button

--- app/src/main/java/com/daniel/dcalendar/MainActivity.java
+++ app/src/main/java/com/daniel/dcalendar/MainActivity.java
@@ -3,6 +3,10 @@ package com.daniel.dcalendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import dcalendar.DDay;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addContentView(new DDay(this,true,true),new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
     }
 }
---
+++ app/src/main/java/dcalendar/DDay.java
@@ -0,0 +1,32 @@
+package dcalendar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.daniel.dcalendar.R;
+
+
+public class DDay extends android.support.v7.widget.AppCompatButton {
+
+    /**
+     * default customize of button - from xml file
+     */
+    private boolean setDefault;
+    private boolean isEvent;
+
+    public DDay(Context context, boolean setDefault, boolean isEvent) {
+        super(context);
+        this.isEvent=isEvent;
+        this.setDefault=setDefault;
+        if(this.setDefault){
+            if(this.isEvent ==true){
+                setBackgroundResource(R.drawable.calendar_week_day_button_pressed);
+            } else{
+                setBackgroundResource(R.drawable.calendar_week_day_button_unpressed);
+            }
+        }
+    }
+    public DDay(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
