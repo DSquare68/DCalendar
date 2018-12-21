1ed234a46dd6149f6ab5dc4852048b7728ddf825
DanielDob <35843305+DanielDob@users.noreply.github.com>
2018-12-21 16:18:54 +0100

    Update events view
    If there isn't event show 'add' button

--- dcalendar/src/main/java/com/daniel/dcalendar/dview/DEventsScroll.java
+++ dcalendar/src/main/java/com/daniel/dcalendar/dview/DEventsScroll.java
@@ -3,20 +3,31 @@ package com.daniel.dcalendar.dview;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
+import com.daniel.dcalendar.R;
+import com.daniel.dcalendar.data.Global;
 import com.daniel.dcalendar.event.DEvent;
+import com.daniel.dcalendar.logic.view.DDayLogic;
 
 public class DEventsScroll extends ScrollView {
     static com.daniel.dcalendar.event.DEvent[] events;
-
+    static TextView info;
+    static Button add;
     static LinearLayout container;
     public DEventsScroll(Context context) {
         super(context);
         container=new LinearLayout(getContext());
         container.setOrientation(LinearLayout.VERTICAL);
-        this.addView(container);
+        this.addView(container, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        setAddButton();
         refreshEvents(getContext());
     }
 
@@ -24,8 +35,30 @@ public class DEventsScroll extends ScrollView {
         super(context,attrs);
         container=new LinearLayout(getContext());
         container.setOrientation(LinearLayout.VERTICAL);
-        this.addView(container);
+        this.addView(container, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT));
+        setAddButton();
         refreshEvents(getContext());
+    }
+
+    private void setAddButton() {
+        info = new TextView(getContext());
+        info.setText(getResources().getString(R.string.no_events));
+        info.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+        ((LinearLayout.LayoutParams) info.getLayoutParams()).gravity=Gravity.CENTER | Gravity.CENTER_VERTICAL;
+
+        add = new Button(this.getContext());
+        add.setText(getResources().getString(R.string.add));
+        add.setLayoutParams( new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT));
+        ((LinearLayout.LayoutParams) add.getLayoutParams()).gravity=Gravity.CENTER | Gravity.CENTER_VERTICAL;
+        add.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Global.yearDEvent=Global.selectedYear;
+                Global.monthDEvent=Global.selectedMonth;
+                Global.dayDEvent=Global.selectedDay;
+                DDayLogic.openDialog(getContext());
+            }
+        });
     }
 
     public static void setEvents(com.daniel.dcalendar.event.DEvent[] event) {
@@ -38,7 +71,15 @@ public class DEventsScroll extends ScrollView {
     }
     public static void refreshEvents(Context context){
         container.removeAllViews();
-        if(events==null) return;
+        if(Global.selectedDay>0){
+            container.addView(info);
+            container.addView(add);
+        }
+        if(events==null||events.length==0) {
+            return;
+
+        }
+        container.removeAllViews();
         sort(events);
         for(int i=0;i<events.length;i++){
             container.addView( new DEventInScroll(context,events[i]));
